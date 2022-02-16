4e41fbf2b9bb4bbb5f2f0da9f45ae7555c913f05
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-02-16 10:44:16 -0800

    esbuild update

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -14,12 +14,12 @@ const ContactUs = () => {
         {/* Fist colum  */}
         <div className=" flex flex-col md:flex-row  relative  px-4 ">
           {/* first row  */}
-          <div className="w-full md:w-1/2 flex items-center">
-            <div>
-              <div className="md:text-4xl text-primary  font-semibold">
-                Connect with us on our socials
+          <div className="w-full relative md:w-1/2 flex items-center">
+            <div className="absolute top-10 left-0 md:relative">
+              <div className="  text-xl md:text-4xl text-primary  font-semibold">
+                Connect with us <br className="md:hidden" /> on our socials
               </div>
-              <div className="text-3xl text-orange flex mb-4 md:mt-4">
+              <div className=" -ml-4 md:ml-0 text-2xl md:text-4xl mt-6 text-orange flex mb-4 md:mt-4">
                 <a href="#" className="footer_link">
                   <FaInstagram />
                 </a>
@@ -41,25 +41,42 @@ const ContactUs = () => {
           {/* second row */}
 
           <div className="w-full md:w-1/2 mt-8 md:mt-0  text-right ">
-            <img src={Boy} alt="connect with us pic" />
+            <img
+              src={Boy}
+              alt="connect with us pic"
+              className="w-72 md:w-full inline"
+            />
           </div>
         </div>
 
         {/* Second colum  */}
-        <div className=" flex flex-col md:flex-row  relative md:mt-20  px-4 ">
+        <div className=" flex flex-col md:flex-row  relative mt-20  px-4 ">
           {/* first row  */}
           <div className="w-full md:w-1/2 mt-8 md:mt-0  text-right ">
-            <img src={Girl} alt="connect with us pic" />
+            <img
+              src={Girl}
+              alt="connect with us pic"
+              className="w-72 md:w-full"
+            />
           </div>
           {/* second row */}
           <div className="w-full md:w-1/2 md:text-4xl font-bold flex items-center justify-end">
             <div>
               <div className=" text-orange  text-right ">Don’t be shy</div>
               <div className="text-primary">
-                say <a href="mailto:hello@rentout.app" className="text-dim" >hello@rentout.app</a>
+                say{" "}
+                <a href="mailto:hello@rentout.app" className="text-dim">
+                  hello@rentout.app
+                </a>
               </div>
             </div>
           </div>
+        </div>
+
+        <div className="mt-20 text-center">
+          <button className="border rounded-lg border-primary py-3 px-4 font-bold text-lg ">
+            Take me home{" "}
+          </button>
         </div>
       </div>
     </div>
